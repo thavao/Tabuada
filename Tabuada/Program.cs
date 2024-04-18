244cb0ec5c580fa60555329ec0194ddd9c137fc8
thavao <othavioact@gmail.com>
2024-04-18 16:02:54 -0300

    Add project files.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,20 @@
+﻿//2 - faça um programa que leia um numero inteiro positivo e imprima a sua tabuada
+
+int numero;
+
+Console.WriteLine("Digite um número inteiro positivo: ");
+numero = int.Parse(Console.ReadLine());
+
+if (numero > 0)
+{
+    Console.WriteLine("Tabuada do " + numero);
+    for (int i = 0; i <= 10; i++)
+    {
+        Console.WriteLine($"{numero} x {i} = {numero * i}");
+    }
+}
+else
+    Console.WriteLine("O valor precisa ser positivo...");
+
+Console.WriteLine("Pressione Enter para encerrar...");
+Console.ReadLine();
